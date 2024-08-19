76e497242262b2e9f0c506bbab17baa48b8aab25
Kubicek, Lee <lkubicek1@gmail.com>
2024-08-19 16:51:59 -0400

    Adding first lesson

---
+++ 01-single/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Welcome to Java!");
+		} else {
+			System.out.println("Welcome to Java, " + args[0] + "!");
+		}
+	}
+}
